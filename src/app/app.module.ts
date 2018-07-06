75f529cad7ac66cae8d73ae345ce24e0bbe43dab
Kirti Kadam <kirtik@synerzipune.local>
2018-07-06 12:13:46 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { jqxGridComponent } from 'jqwidgets-scripts/jqwidgets-ts/angular_jqxgrid';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    jqxGridComponent
   ],
   imports: [
     BrowserModule
